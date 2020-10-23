70f49114538b8ba7a2b6c4c9cf42381013010052
Jordi Enric <je.roig@innovation.es>
2020-10-23 16:01:39 +0200

    update carta

--- src/pages/Carta.vue
+++ src/pages/Carta.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="menu">
-        <h1>Parada d'9 Carta</h1>
+        <div class="title">
+            <g-image height="55" width="55" src="~/assets/logo.svg"></g-image>
+            <h1>Parada d'9 Carta</h1>
+        </div>
         <h2 v-if="loading">Cargando...</h2>
         <div class="menu-container" v-for="(item, index) in menus" :key="index">
             <h3>{{item.title}}</h3>
@@ -12,6 +15,15 @@
                 </div>
             </div>
         </div>
+        <footer>
+            <a target="_blank" class="instagram-images" href="https://www.instagram.com/parada.d9/">
+                <g-image width="100%" src="~/assets/img/plat.jpg"></g-image>
+                <g-image width="100%" src="~/assets/img/burger.jpg"></g-image>
+                <g-image width="100%" src="~/assets/img/tostada.jpg"></g-image>
+            </a>
+            <a target="_blank" class="cta" href="https://www.instagram.com/parada.d9/">¡Síguenos en Instagram!</a>
+            <a target="_blank" class="cta" href="https://www.instagram.com/parada.d9/">@parada.d9</a>
+        </footer>
     </div>
 </template>
 
@@ -49,8 +61,21 @@ export default {
     max-width: 900px;
     margin: auto;
     padding: 1rem;
+    margin-bottom: 4rem;
+    h1, h2, h3 {
+        margin: 0;
+        padding: .5rem 0;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        color: #2e3041;
+    }
+    .title {
+        text-align: center;
+        border-bottom: 1px solid #eaeaea;
+        margin-bottom: 1rem;
+    }
     .row {
         display: flex;
+        margin-bottom: 4rem;
         .col {
             text-align: right;
         }
@@ -59,15 +84,47 @@ export default {
             flex-grow: 1;
         }
         .item {
-            padding: .25rem 1rem;
+            padding: 0 .25rem;
             min-height: 55px;
+            border-bottom: 1px solid #eaebfa;
             &:first-of-type {
-                font-weight: bold;
+                min-height: 33px;
+                color: #434343;
+                font-weight: lighter;
+                border-bottom: none;
             }
             &:nth-of-type(2n) {
-                background-color: rgb(235, 235, 235);
+                // background-color: rgb(244, 245, 250);
             }
         }
+    }
+}
+
+footer {
+    border: dashed 2px #FED7D7;
+    background-color: #FFF5F5;
+    color: #3f0d0d;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    border-radius: 1rem;
+    margin: 1rem auto;
+    padding: 1rem;
+
+    .instagram-images {
+        display: flex;
+        justify-content: space-between;
+        img {
+            border-radius: .5rem;
+            border: 1px solid #FED7D7;
+        }
+    }
+    .cta {
+        padding: 1rem;
+        text-align: center;
+        display: block;
+        text-decoration: none;
+        font-size: 1.4rem;
+        color: #3f0d0d;
+        font-weight: bold;
     }
 }
 </style>
